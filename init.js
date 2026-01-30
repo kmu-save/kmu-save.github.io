6483e0cf4c69d2c1c8ebb0a635e62dd295de36d5
elecbug <deveb1479@gmail.com>
2026-01-30 17:23:22 +0900

    feat: add footer

---
+++ init.js
@@ -0,0 +1,7 @@
+footer = `
+<p>
+    © 2026 <a href="https://github.com/elecbug">elecbug</a> in <a href="https://github.com/kmu-save">KMU SAVE</a>. All rights reserved.
+</p>
+`;
+
+document.getElementById('footer').innerHTML = footer;
