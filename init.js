6ac742d8703e3968bf7a621ecadd6d8323a87fb6
elecbug <deveb1479@gmail.com>
2026-01-31 20:19:54 +0900

    feat: update style

--- init.js
+++ init.js
@@ -1,8 +1,7 @@
 header = `
 <h1>
     <a href="/" style="text-decoration: none; color: inherit;">
-        <img src="/figure/icon.png" alt="Article SAVE Logo" style="height: 50px; vertical-align: middle; margin-right: 10px;">
-        Article SAVE
+        <img src="/figure/icon.png" alt="Article SAVE Logo" style="height: 50px; vertical-align: middle; margin-right: 10px;">Article SAVE
     </a>
 </h1>
 `;
@@ -14,17 +13,23 @@ footer = `
 `;
 
 nav = `
-<h3 style="margin-bottom: 10px; text-align: center;">
+<p style="margin-top: 0px;margin-bottom: 10px; text-align: center; font-weight: bold; font-size: 1.2em;">
     MENU
-</h3>
+</p>
 <div class="nav-separator"></div>
 <div>
 <table style="width: 100%; border-collapse: collapse; margin-top: 10px;">
     <tr>
-        <td><button class="menu-button" id="home-button">홈</button></td>
+        <td><button class="menu-button" id="home-button">Home</button></td>
     </tr>
     <tr>
-        <td><button class="menu-button" id="submission-procedure-button">투고 절차 안내</button></td>
+        <td><button class="menu-button" id="submission-procedure-button">Submission Procedure</button></td>
+    </tr>
+    <tr>
+        <td><button class="menu-button" id="article-college-button"><b>Article</b> - College</button></td>
+    </tr>
+    <tr>
+        <td><button class="menu-button" id="article-computer-button"><b>Article</b> - Computer</button></td>
     </tr>
 </table>
 </div>
@@ -41,4 +46,12 @@ document.getElementById('home-button').addEventListener('click', function() {
 
 document.getElementById('submission-procedure-button').addEventListener('click', function() {
     window.location.href = '/submission-procedure';
+});
+
+document.getElementById('article-college-button').addEventListener('click', function() {
+    window.location.href = '/article/college/overview';
+});
+
+document.getElementById('article-computer-button').addEventListener('click', function() {
+    window.location.href = '/article/computer/overview';
 });
